e4f00f4d8d0439f0f473e3d085813c851d8cbde4
Michael Klein <michael@firstiwaslike.com>
2021-09-21 11:05:12 +0200

    feat: add docs for button component

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,10 +1,16 @@
 import { hbs } from 'ember-cli-htmlbars';
 import { action } from '@storybook/addon-actions';
+import ButtonDocs from './button.mdx';
 
 // the default export tells storybook where to put it in terms of navigation
 export default {
   title: 'Button',
   component: 'Button',
+  parameters: {
+    docs: {
+      page: ButtonDocs,
+    },
+  },
 };
 
 export const Basic = () => ({
